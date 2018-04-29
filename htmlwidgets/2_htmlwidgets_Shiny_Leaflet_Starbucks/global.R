13c8b2d97df9ceb1ba666709630c48ffa98c89b2
philbowsher <indyprb@gmail.com>
2018-04-29 19:01:28 +0000

    path sb shiny

--- htmlwidgets/2_htmlwidgets_Shiny_Leaflet_Starbucks/global.R
+++ htmlwidgets/2_htmlwidgets_Shiny_Leaflet_Starbucks/global.R
@@ -1,1 +1,1 @@
-data <- read.csv("~/shiny-rmd-APIs-pharma-session-2017-11-08/2_htmlwidgets_Shiny_Leaflet_Starbucks/data.csv", header=TRUE)
+data <- read.csv("~/PharmaSUG-2018-Intro-R-4-Drug-Development-Workshop/htmlwidgets/2_htmlwidgets_Shiny_Leaflet_Starbucks/data.csv", header=TRUE)
